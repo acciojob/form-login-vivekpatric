ac92c229851cac6028bc07a33ee520cbc32d1549
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-06-01 12:07:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+   let firstname=document.getElementsByTagName("input")[0];
+	let secondname=document.getElementsByTagName("input")[1];
+	alert(firstname.value+" "+secondname.value);
 }
